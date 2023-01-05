7e93b6a912dd7a72ead58b2b991a54e0c2fb9bf0
senazeleke <105046324+senazeleke@users.noreply.github.com>
2023-01-05 15:15:29 +0300

    Update Soap.java

--- Quiz1/Soap.java
+++ Quiz1/Soap.java
@@ -1,5 +1,16 @@
 package soap;
 
 public class Soap {
-
+  void Detergent()
+  {
+    System.out.println("keep things clean even the floor");
+                       }
+  public static void main(String[] args)
+  {
+    Duru dur = new Duru();
+    dur.forcloth();
+    dur.forbath();
+    dur.cleanhouse();
+   dur.Detergent()
+  }
 }
